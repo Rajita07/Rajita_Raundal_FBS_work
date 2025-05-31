1831fb9f0dbff37b5a04463b6766687c7ee71007
Rajita07 <rajita.raundal.07@gmail.com>
2025-06-01 02:08:08 +0530

    Delete Assignmets directory

--- Assignmets/Assignment 0/AreaOfTriangle.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float b=10, h=20, area;
-    area = (b*h)/2;
-    printf("Area of Triangle = %f", area);
-}
--- Assignmets/Assignment 0/Average.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a=10,b=20,c=30,d=40,e=50;
-    float average;
-
-    average = (a+b+c+d+e)/5;
-    printf("average = %f", average);
-}
